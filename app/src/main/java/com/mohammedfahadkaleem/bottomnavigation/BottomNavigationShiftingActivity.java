1d34414e8f6647f312c3eff7d8c7dde2ae558720
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2018-03-15 17:57:50 -0500

    + Shifting Bottom Navigation Bar

--- app/src/main/java/com/mohammedfahadkaleem/bottomnavigation/BottomNavigationShiftingActivity.java
+++ app/src/main/java/com/mohammedfahadkaleem/bottomnavigation/BottomNavigationShiftingActivity.java
@@ -1,13 +1,47 @@
 package com.mohammedfahadkaleem.bottomnavigation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class BottomNavigationShiftingActivity extends AppCompatActivity {
-
+  private BottomNavigationView shiftinNavigationView;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_bottom_navigation_shifting);
+    init();
+  }
+
+  private void init(){
+    shiftinNavigationView = findViewById(R.id.bottom_navigation_shifting);
+    shiftinNavigationView.setOnNavigationItemSelectedListener(new OnNavigationItemSelectedListener() {
+      @Override
+      public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+          case R.id.navigation_movie:
+            Toast.makeText(getApplicationContext(),R.string.movie_amp_tv_clicked,Toast.LENGTH_SHORT).show();
+            shiftinNavigationView.setBackgroundColor(getResources().getColor(R.color.blueGrey));
+            return true;
+          case R.id.navigation_music:
+            Toast.makeText(getApplicationContext(),R.string.music_clicked,Toast.LENGTH_SHORT).show();
+            shiftinNavigationView.setBackgroundColor(getResources().getColor(R.color.pink_700));
+            return true;
+          case R.id.navigation_books:
+            Toast.makeText(getApplicationContext(),R.string.books_clicked,Toast.LENGTH_SHORT).show();
+            shiftinNavigationView.setBackgroundColor(getResources().getColor(R.color.indigo_700));
+            return true;
+          case R.id.navigation_newsstand:
+            Toast.makeText(getApplicationContext(),R.string.newsstand_clicked,Toast.LENGTH_SHORT).show();
+            shiftinNavigationView.setBackgroundColor(getResources().getColor(R.color.light_green_700));
+            return true;
+        }
+        return false;
+      }
+    });
   }
 }
